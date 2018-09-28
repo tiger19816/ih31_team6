cfac6348c9e5aaefd138c98b55db4be9cc90f21b
tiger19816 <tiger19816@gmail.com>
2018-09-28 10:35:26 +0900

    commit

--- sugukuru/sugukuru/ClaimCollection/IndividualReconciliation.Designer.cs
+++ sugukuru/sugukuru/ClaimCollection/IndividualReconciliation.Designer.cs
@@ -28,77 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.order_no = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.employee_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.client_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.car_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.claim_amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.payment_amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.addButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.order_no,
-            this.employee_name,
-            this.client_name,
-            this.car_name,
-            this.claim_amount,
-            this.payment_amount});
-            this.dataGridView1.Location = new System.Drawing.Point(23, 69);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(1120, 496);
-            this.dataGridView1.TabIndex = 2;
-            // 
-            // order_no
-            // 
-            this.order_no.HeaderText = "受注番号";
-            this.order_no.Name = "order_no";
-            // 
-            // employee_name
-            // 
-            this.employee_name.HeaderText = "担当者名";
-            this.employee_name.Name = "employee_name";
-            // 
-            // client_name
-            // 
-            this.client_name.HeaderText = "顧客名";
-            this.client_name.Name = "client_name";
-            // 
-            // car_name
-            // 
-            this.car_name.HeaderText = "車名";
-            this.car_name.Name = "car_name";
-            // 
-            // claim_amount
-            // 
-            this.claim_amount.HeaderText = "請求額";
-            this.claim_amount.Name = "claim_amount";
-            // 
-            // payment_amount
-            // 
-            this.payment_amount.HeaderText = "支払額";
-            this.payment_amount.Name = "payment_amount";
-            // 
-            // addButton
-            // 
-            this.addButton.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.addButton.Location = new System.Drawing.Point(525, 569);
-            this.addButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(117, 53);
-            this.addButton.TabIndex = 3;
-            this.addButton.Text = "追加";
-            this.addButton.UseVisualStyleBackColor = true;
-            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
             // button1
             // 
@@ -121,14 +55,22 @@
             this.label1.TabIndex = 99;
             this.label1.Text = "個別消込一覧";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(505, 407);
+            this.dataGridView1.TabIndex = 102;
+            // 
             // IndividualReconciliation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.addButton);
-            this.Controls.Add(this.dataGridView1);
             this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "IndividualReconciliation";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -138,16 +80,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn order_no;
-        private System.Windows.Forms.DataGridViewTextBoxColumn employee_name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn client_name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn car_name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn claim_amount;
-        private System.Windows.Forms.DataGridViewTextBoxColumn payment_amount;
-        private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
