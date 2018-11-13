76fd9ea89928ea219edcdca8ddd13af571d4db0a
ohiHALoskaka2018 <40971214+ohiHALoskaka2018@users.noreply.github.com>
2018-11-13 18:17:30 +0900

    落札車両途中

--- sugukuru/sugukuru/Purchase/BitCarList.cs
+++ sugukuru/sugukuru/Purchase/BitCarList.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace sugukuru.Purchase
 {
     public partial class BitCarList : FormMaster.UserControlMST
     {
+
+        //DB接続文字列の取得
+        string conStr;
+
         public BitCarList()
         {
             InitializeComponent();
+            this.conStr = ConfigurationManager.AppSettings["DbConKey"];
+
+            //SQL文を発行する
+            DataSet dset1 = new DataSet("order");
+            DataSet dset2 = new DataSet("bid");
+            MySqlConnection con = new MySqlConnection(this.conStr);
+            con.Open();
+
+            //sql文
+
         }
     }
 }
