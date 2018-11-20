9b0f7c47a55791a50a8504007b29b537add5f346
tiger19816 <tiger19816@gmail.com>
2018-11-20 10:23:13 +0900

    Update BulkReconciliation.cs

--- sugukuru/sugukuru/ClaimCollection/BulkReconciliation.cs
+++ sugukuru/sugukuru/ClaimCollection/BulkReconciliation.cs
@@ -30,16 +30,16 @@ namespace sugukuru.ClaimCollection
 
             //はじめのファイル名を指定する
             //はじめに「ファイル名」で表示される文字列を指定する
-            ofd.FileName = "default.html";
+            ofd.FileName = "default.csv";
             //はじめに表示されるフォルダを指定する
             //指定しない（空の文字列）の時は、現在のディレクトリが表示される
             ofd.InitialDirectory = @"C:\";
             //[ファイルの種類]に表示される選択肢を指定する
             //指定しないとすべてのファイルが表示される
-            ofd.Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+            ofd.Filter = "CSVファイル(*.html;*.htm)|*.csv|すべてのファイル(*.*)|*.*";
             //[ファイルの種類]ではじめに選択されるものを指定する
             //2番目の「すべてのファイル」が選択されているようにする
-            ofd.FilterIndex = 2;
+            //ofd.FilterIndex = 2;
             //タイトルを設定する
             ofd.Title = "開くファイルを選択してください";
             //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
@@ -54,8 +54,7 @@ namespace sugukuru.ClaimCollection
             //ダイアログを表示する
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                //OKボタンがクリックされたとき、選択されたファイル名を表示する
-                Console.WriteLine(ofd.FileName);
+                tbFileName.Text = ofd.FileName;
             }
         }
 
