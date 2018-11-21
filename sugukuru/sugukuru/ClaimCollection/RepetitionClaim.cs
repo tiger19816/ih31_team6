170d647e6ef842ae14196f0c3f6e1137fd76d13e
tiger19816 <tiger19816@gmail.com>
2018-11-21 10:57:04 +0900

    commit

--- sugukuru/sugukuru/ClaimCollection/RepetitionClaim.cs
+++ sugukuru/sugukuru/ClaimCollection/RepetitionClaim.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace sugukuru.ClaimCollection
 {
     public partial class RepetitionClaim : FormMaster.UserControlMST
     {
+        //DB接続文字列の取得
+        string conStr;
+
         public RepetitionClaim()
         {
             InitializeComponent();
+            this.conStr = ConfigurationManager.AppSettings["DbConKey"];
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
