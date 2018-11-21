e9b72f20c4db341a340097e223cdb1da2a90c535
tiger19816 <tiger19816@gmail.com>
2018-11-21 11:23:34 +0900

    Update RepetitionClaim.cs

--- sugukuru/sugukuru/ClaimCollection/RepetitionClaim.cs
+++ sugukuru/sugukuru/ClaimCollection/RepetitionClaim.cs
@@ -25,7 +25,29 @@ namespace sugukuru.ClaimCollection
 
         private void btSearch_Click(object sender, EventArgs e)
         {
+            string sql = "SELECT bc.no AS b_no, SUM(bd.quantity * bd.unit_price) AS price "
+                    + "FROM billing_clearing bc "
+                    + "INNER JOIN bill b ON bc.no = b.invoice_number "
+                    + "INNER JOIN client c ON b.customer_id = c.id "
+                    + "INNER JOIN billing_detail bd ON b.invoice_number = bd.invoice_number "
+                    + "WHERE bc.clearing_flag = 2";
 
+            //DB接続オブジェクトを作成
+            MySqlConnection con = new MySqlConnection(this.conStr);
+
+            //DB接続
+            con.Open();
+
+            Console.WriteLine(sql);
+
+            //抽象データ格納データセットを作成
+            DataSet dset = new DataSet("bill");
+
+            //データアダプターの生成
+            MySqlDataAdapter mAdp = new MySqlDataAdapter(sql, con);
+
+            ///データ抽出＆取得
+            mAdp.Fill(dset, "bill");
         }
     }
 }
